b350ed5693649b5854d60156f6e13877a296d420
devBernardo-Mendes <devbernardomr@gmail.com>
2022-01-03 22:11:23 -0300

    feat: :sparkles: Running TypeScript - Manual

---
+++ introdução/basic.js
@@ -0,0 +1,6 @@
+var a = "Teste TS!";
+console.log(a);
+
+// -----------------------------------------------------
+// Nessa aula monstra como ultilzar o terminal para rodar o codigo TS, onde tranformar o codigo TS em JS, conforme os arquivos desta aula.
+// -----------------------------------------------------
---
+++ introdução/basic.ts
@@ -0,0 +1,7 @@
+const a: string = "Teste TS!";
+
+console.log(a);
+
+// -----------------------------------------------------
+// Nessa aula monstra como ultilzar o terminal para rodar o codigo TS, onde tranformar o codigo TS em JS, conforme os arquivos desta aula.
+// -----------------------------------------------------
